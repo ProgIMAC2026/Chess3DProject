26be5118da9b81ed2d5fa37369b6f4ba54a810bd
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-03-24 12:40:37 +0100

    ✨ extend Object class with position, rotation, scale, and resource pointers

--- src/renderer/scene/object/Object.hpp
+++ src/renderer/scene/object/Object.hpp
@@ -1,3 +1,34 @@
 #pragma once
 
-class Object {};
+#include <lib/glm/glm.hpp>
+#include "../shader/ShaderProgram.hpp"
+#include "../texture/Texture.hpp"
+#include "Mesh.hpp"
+
+class Object {
+    glm::vec3 position;
+    glm::vec3 rotation;
+    glm::vec3 scale;
+
+    Mesh*          meshPtr;
+    ShaderProgram* shaderProgram;
+    Texture*       texture;
+
+public:
+    // Getter Setter
+    glm::vec3 getPosition() const;
+    void      setPosition(const glm::vec3& position);
+    glm::vec3 getRotation() const;
+    void      setRotation(const glm::vec3& rotation);
+    glm::vec3 getScale() const;
+    void      setScale(const glm::vec3& scale);
+
+    Mesh* getMeshPtr() const;
+    void  setMeshPtr(Mesh* meshPtr);
+
+    ShaderProgram* getShaderProgram() const;
+    void           setShaderProgram(ShaderProgram* shaderProgram);
+
+    Texture* getTexture() const;
+    void     setTexture(Texture* texture);
+};
