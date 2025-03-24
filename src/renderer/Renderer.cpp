04624e979896b1e619ad6f734878355a4edcdeda
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-03-24 11:47:07 +0100

    ✨ update Renderer class to accept Window dependency and initialize GLFW context

--- src/renderer/Renderer.cpp
+++ src/renderer/Renderer.cpp
@@ -1,7 +1,19 @@
 #include "Renderer.hpp"
+#include "GLFW/glfw3.h"
+#include "Window.hpp"
 
-Renderer::Renderer() {};
+Renderer::Renderer(Window& window)
+{
+    init();
+    window.create();
+    glfwMakeContextCurrent(window.getWindow());
+};
 
 Renderer::~Renderer() {};
+
+void Renderer::init()
+{
+    glfwInit();
+};
 
 void Renderer::render(ChessGame& chessGame) {};
--- src/renderer/Renderer.hpp
+++ src/renderer/Renderer.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include "Window.hpp"
 #include "chessgame/ChessGame.hpp"
 class Renderer {
+private:
+    void init();
+
 public:
-    Renderer();
+    Renderer() = default;
+    Renderer(Window& window);
     ~Renderer();
+
     void render(ChessGame& chessGame);
 };
