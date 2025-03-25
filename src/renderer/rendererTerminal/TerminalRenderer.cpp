910904022c5fac46f9e6be0348f609c401c63519
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-03-25 12:01:09 +0100

    ✨ add possible move visualization in terminal renderer

--- src/renderer/rendererTerminal/TerminalRenderer.cpp
+++ src/renderer/rendererTerminal/TerminalRenderer.cpp
@@ -6,6 +6,13 @@ void TerminalRenderer::render(ChessGame& chessGame)
 {
     while (true)
     {
+        // Get possible moves
+        std::vector<ChessTile*> possibleMoves = std::vector<ChessTile*>();
+        if (chessGame.getSelectedTile() != nullptr && chessGame.getSelectedTile()->getPiece() != nullptr)
+        {
+            possibleMoves = chessGame.getSelectedTile()->getPiece()->getPossibleMoves();
+        }
+
         // Clear terminal
         system("cls");
         // Render chess board in terminal
@@ -14,6 +21,16 @@ void TerminalRenderer::render(ChessGame& chessGame)
             for (int j = 0; j < 8; j++)
             {
                 auto tile = chessGame.getChessBoard().getTile({i, j});
+
+                if (tile == chessGame.getSelectedTile() || std::find(possibleMoves.begin(), possibleMoves.end(), tile) != possibleMoves.end())
+                {
+                    std::cout << "[";
+                }
+                else
+                {
+                    std::cout << " ";
+                }
+
                 if (tile->getPiece() != nullptr)
                 {
                     std::cout << tile->getPiece()->getSymbol();
@@ -21,6 +38,15 @@ void TerminalRenderer::render(ChessGame& chessGame)
                 else
                 {
                     std::cout << ".";
+                }
+
+                if (tile == chessGame.getSelectedTile() || std::find(possibleMoves.begin(), possibleMoves.end(), tile) != possibleMoves.end())
+                {
+                    std::cout << "]";
+                }
+                else
+                {
+                    std::cout << " ";
                 }
             }
             std::cout << std::endl;
