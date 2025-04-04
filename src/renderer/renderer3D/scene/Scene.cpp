1b64080d53d81e71ec3acf4ded06d784e2f3baf9
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-04-04 13:25:22 +0200

    ✨ enhance Scene class with object and light management methods, including constructors for initialization

--- src/renderer/renderer3D/scene/Scene.cpp
+++ src/renderer/renderer3D/scene/Scene.cpp
@@ -2,8 +2,28 @@
 
 void Scene::init()
 {
-    for (auto& object : objects)
-        object.init();
     for (auto& light : lights)
         light.init();
+}
+
+void Scene::addObject(Object object)
+{
+    objects.push_back(object);
+}
+
+void Scene::addLight(const Light& light) {}
+
+std::vector<Object>& Scene::getObjects()
+{
+    return objects;
+}
+
+std::vector<Light>& Scene::getLights()
+{
+    return lights;
+}
+
+Camera& Scene::getCamera()
+{
+    return camera;
 }
--- src/renderer/renderer3D/scene/Scene.hpp
+++ src/renderer/renderer3D/scene/Scene.hpp
@@ -1,13 +1,34 @@
 #pragma once
 
+#include <utility>
 #include <vector>
+#include "camera/Camera.hpp"
 #include "light/Light.hpp"
 #include "object/Object.hpp"
+#include "renderer/renderer3D/shader/ShaderProgram.hpp"
 
 class Scene {
     std::vector<Object> objects;
     std::vector<Light>  lights;
+    Camera              camera;
+    glm::mat4           modelMatrix = glm::mat4(1.0f);
 
 public:
+    Scene()
+        : camera(CameraParameters{.position = glm::vec3(0.f, 0.5f, 0.5f), .target = glm::vec3(0.f, 0.f, 0.f), .up = glm::vec3(0.f, 1.f, 0.f), .fov = 45.f, .aspect = 800.f / 600.f, .near = 0.1f, .far = 100.f}) {};
+
+    Scene(std::vector<Object> objects, std::vector<Light> lights, Camera camera)
+        : objects(std::move(objects)), lights(std::move(lights)), camera(camera) {};
+
     void init();
+
+    void addObject(Object object);
+    void addLight(const Light& light);
+
+    std::vector<Object>& getObjects();
+    std::vector<Light>&  getLights();
+
+    Camera& getCamera();
+
+    glm::mat4 getModelMatrix() const { return modelMatrix; }
 };
