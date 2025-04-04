122469aa2f6ebdd4db1c4e9daaad46cff4eee75d
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-04-04 13:27:13 +0200

    ✨ enhance Renderer3D with shader program initialization and scene rendering capabilities

--- src/renderer/renderer3D/Renderer3D.cpp
+++ src/renderer/renderer3D/Renderer3D.cpp
@@ -1,26 +1,92 @@
 #include "Renderer3D.hpp"
-
-void Renderer3D::init()
-{
-    glfwInit();
-};
+#include <filesystem>
+#include <lib/glm/ext.hpp>
+#include "../../utils/errors/Exception.hpp"
+#include "loader/meshLoading.hpp"
+#include "scene/object/Mesh.hpp"
+#include "scene/object/Object.hpp"
+#include "scene/utils/Transform.hpp"
+#include "shader/ShaderProgram.hpp"
 
 Renderer3D::Renderer3D()
-    : scene(), window(400, 400)
+    : scene(), window(800, 600), shaderProgram(nullptr)
 {
     glfwInit();
     window.create();
     glfwMakeContextCurrent(window.getWindow());
 
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    meshLoader.LoadAllMeshes();
+
+    std::filesystem::path ressources_path = "C:/Users/colin/Desktop/IMAC/IMAC_2/S2/PROG/Chess3DProject/res";
+    // Load the shaders
+
+    shaderProgram = new ShaderProgram(
+        ressources_path / "shaders" / "vertex.glsl",
+        ressources_path / "shaders" / "fragment.glsl"
+    );
+
+    shaderProgram->link();
+
+    Object object(
+        Transform(),
+        meshLoader.getMesh("knight"),
+        shaderProgram,
+        nullptr
+    );
+
+    scene.addObject(object);
 };
 
 void Renderer3D::render(ChessGame& chessGame)
 {
     scene.init();
+    glEnable(GL_DEPTH_TEST);
     while (!window.shouldClose())
     {
         window.pollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        renderScene();
+
         window.swapBuffers();
     }
 };
+
+void Renderer3D::renderScene()
+{
+    if (!shaderProgram)
+    {
+        throw ShaderException("Shader program is not initialized in " + std::string(__FUNCTION__));
+    }
+    // Set the shader program to use
+
+    // Set the view and projection matrices
+    glUseProgram(shaderProgram->getId());
+    shaderProgram->uniformMatrix4fv("viewMatrix", glm::value_ptr(scene.getCamera().getViewMatrix()));
+    shaderProgram->uniformMatrix4fv("projectionMatrix", glm::value_ptr(scene.getCamera().getProjectionMatrix()));
+
+    // Set the model matrix
+
+    // Set the camera position
+    shaderProgram->uniform3fv("viewPos", glm::value_ptr(scene.getCamera().getPosition()));
+
+    // Render each object in the scene
+    for (Object& obj : scene.getObjects())
+    {
+        shaderProgram->uniformMatrix4fv("modelMatrix", glm::value_ptr(obj.getModelMatrix()));
+        renderObject(obj);
+    }
+}
+
+void Renderer3D::renderObject(Object& object)
+{
+    glBindVertexArray(object.getMeshPtr()->getVAO());
+    // Draw the model
+    glDrawElements(GL_TRIANGLES, object.getMeshPtr()->getIndicesSize(), GL_UNSIGNED_SHORT, 0);
+    // Unbind the VAO
+    glBindVertexArray(0);
+}
--- src/renderer/renderer3D/Renderer3D.hpp
+++ src/renderer/renderer3D/Renderer3D.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
+#include "loader/MeshLoader.hpp"
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include "../Renderer.hpp"
 #include "scene/Scene.hpp"
+#include "shader/ShaderProgram.hpp"
 #include "window/Window.hpp"
 
 class Renderer3D : public Renderer {
 private:
     Scene  scene;
     Window window;
-    void   init();
+
+    ShaderProgram* shaderProgram;
+
+    MeshLoader meshLoader;
 
 public:
     Renderer3D();
@@ -19,4 +24,5 @@ public:
     void render(ChessGame& chessGame) override;
 
     void renderScene();
+    void renderObject(Object& object);
 };
