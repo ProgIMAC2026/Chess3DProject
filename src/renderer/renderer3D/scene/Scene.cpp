494f8108c549b659617a0b777d28f929841679a9
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-04-05 11:03:24 +0200

    ✨ add createChessGameScene method to Scene class for initializing chess game scenes

--- src/renderer/renderer3D/scene/Scene.cpp
+++ src/renderer/renderer3D/scene/Scene.cpp
@@ -1,4 +1,7 @@
 #include "Scene.hpp"
+#include <vector>
+#include "chessgame/piece/Piece.hpp"
+#include "utils/Transform.hpp"
 
 void Scene::addObject(Object object)
 {
@@ -20,4 +23,33 @@ std::vector<Light>& Scene::getLights()
 Camera& Scene::getCamera()
 {
     return camera;
+}
+
+Scene Scene::createChessGameScene(ChessGame& chessGame, MeshChessLoader& meshLoader, ShaderProgram& shaderProgram)
+{
+    std::vector<Piece*> pieces = chessGame.getChessBoard().getPieces();
+    Scene               scene;
+    for (Piece* piece : pieces)
+    {
+        // Create object from piece
+        Object object(
+            Transform(
+                positionTileTo3DPosition(piece->getPosition()),
+                glm::vec3(0.f),
+                glm::vec3(1.f)
+            ),
+            meshLoader.getMesh(piece->getType()),
+            &shaderProgram,
+            nullptr // TODO(colin): Create texture from piece
+        );
+
+        scene.addObject(object);
+    }
+
+    return scene;
+}
+
+glm::vec3 Scene::positionTileTo3DPosition(const PositionTile& positionTile)
+{
+    return glm::vec3(positionTile.x * 1.f / 8, 0.f, positionTile.y * 1.f / 8) - glm::vec3(0.5f, 0.f, 0.5f); // Center the board at (0, 0, 0)
 }
--- src/renderer/renderer3D/scene/Scene.hpp
+++ src/renderer/renderer3D/scene/Scene.hpp
@@ -3,8 +3,10 @@
 #include <utility>
 #include <vector>
 #include "camera/Camera.hpp"
+#include "chessgame/ChessGame.hpp"
 #include "light/Light.hpp"
 #include "object/Object.hpp"
+#include "renderer/renderer3D/loader/MeshChessLoader.hpp"
 #include "renderer/renderer3D/shader/ShaderProgram.hpp"
 
 class Scene {
@@ -15,7 +17,7 @@ class Scene {
 
 public:
     Scene()
-        : camera(CameraParameters{.position = glm::vec3(0.f, 0.5f, 0.5f), .target = glm::vec3(0.f, 0.f, 0.f), .up = glm::vec3(0.f, 1.f, 0.f), .fov = 45.f, .aspect = 800.f / 600.f, .near = 0.1f, .far = 100.f}) {};
+        : camera(CameraParameters{.position = glm::vec3(1.2f, 0.5f, 1.2f), .target = glm::vec3(0.f, 0.f, 0.f), .up = glm::vec3(0.f, 1.f, 0.f), .fov = 45.f, .aspect = 800.f / 600.f, .near = 0.1f, .far = 100.f}) {};
 
     Scene(std::vector<Object> objects, std::vector<Light> lights, Camera camera)
         : objects(std::move(objects)), lights(std::move(lights)), camera(camera) {};
@@ -31,4 +33,10 @@ public:
     // Setters
     void addObject(Object object);
     void addLight(const Light& light);
+
+    // Create scene from chessgame
+    static Scene createChessGameScene(ChessGame& chessGame, MeshChessLoader& meshLoader, ShaderProgram& shaderProgram);
+
+    // Convert PositionTile to 3D position
+    static glm::vec3 positionTileTo3DPosition(const PositionTile& positionTile);
 };
